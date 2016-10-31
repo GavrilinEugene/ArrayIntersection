2b4ea219fc6c02d001eedae36a2e2f58283eddf1
EugeneGavrilin <julentver1@gmail.com>
2016-10-31 20:04:23 +0300

    Поиск пересечения двух массивов типа double. Замена на множества. Небольшая переработка кода

---
+++ src/Set.java
@@ -0,0 +1,74 @@
+import java.util.Arrays;
+
+
+public class Set {
+    public static final double defaultEpsilon = 0.001;
+
+    public static double[] intersection(double[] firstSet, double[] secondSet, double epsilon){
+
+        double[] intersectionSet = new double[firstSet.length];
+        int intersectionSetLength = 0;
+
+        for(int firstSetIndex = 0; firstSetIndex < firstSet.length; firstSetIndex ++){
+            for(int secondSetIndex = 0; secondSetIndex < secondSet.length; secondSetIndex++){
+                if(checkEqualityWithEpsilon(firstSet[firstSetIndex],secondSet[secondSetIndex], epsilon))
+                    intersectionSet[intersectionSetLength ++] = firstSet[firstSetIndex];
+            }
+        }
+        return cutSetByLength(intersectionSet, intersectionSetLength);
+    }
+
+    private static boolean checkEqualityWithEpsilon(double firstElement, double secondElement, double epsilon){
+        return Math.abs(firstElement - secondElement) < epsilon;
+    }
+
+    private static double[] cutSetByLength(double[] set, int length){
+        double[] ret = new double[length];
+        System.arraycopy(set, 0, ret, 0, length);
+        return ret;
+    }
+
+
+    private static boolean checkTests() {
+        System.out.print("running tests: ");
+        boolean testResult = testArrayIntersection();
+        if(testResult == true)
+            System.out.println("TRUE");
+        else
+            System.out.println("FALSE");
+        return testResult;
+    }
+
+    private static boolean testArrayIntersection(){
+        return Arrays.equals(intersection(new double[]{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0},
+                                          new double[]{2.0, 4.0, 6.0, 8.0},
+                                          defaultEpsilon),
+                                          new double[]{2.0,4.0,6.0});
+
+    }
+
+    public static void printSet(double[] set){
+        System.out.print("{");
+        for(double element: set)
+            System.out.print(element + " ");
+        System.out.println("}");
+    }
+
+    public static void main(String[] args){
+
+        boolean passTests = checkTests();
+        if(!passTests)
+            return;
+
+        double[] array1 = {2.5, 3.0, 4.1, 5.5, 6.6, 7.9};
+        double[] array2 = {2.3, 4.1, 6.6, 7.9};
+        double[] result = intersection(array1,array2, defaultEpsilon);
+
+        System.out.println("SourceArrays:");
+        printSet(array1);
+        printSet(array2);
+
+        System.out.println("Intersection:");
+        printSet(result);
+    }
+}
